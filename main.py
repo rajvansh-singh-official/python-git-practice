484e948b6df526e682b45e3f707045dc1b609ec4
Rajvansh Singh <rajvanshsingh1357@gmail.com>
2025-10-07 19:05:39 +0530

    added farewell function

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 def greet(name):
-    return f"Hello, {name}!"
+  return f"Hello, {name}!"
+
+def farewell(name):
+  return f"Goodbye, {name}"  
 
 name = input("Enter your name: ")
 print(greet(name))
